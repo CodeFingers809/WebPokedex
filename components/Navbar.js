6a55c6ab0cd86f44bab29fa7b22efaedf3b4971d
DragonSoup <hereandthere809@gmail.com>
2022-05-15 04:31:00 +0530

    added meta tags

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="bg-poke-red shadow-[0_4px_50px_#EF5350] h-14 p-2 flex align-middle justify-center">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <Link href="/"><a><img src="/logo.png" alt="" className="h-9 ml-4" /></a></Link>
+      <Link href="/"><a><img src="/logo.webp" alt="" className="h-9 ml-4" /></a></Link>
     </div>
   );
 };
